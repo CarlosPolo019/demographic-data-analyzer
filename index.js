488c44e0c0f16cf0b910fcce719867ac3d640ffa
Carlos Manuel Escorcia Polo <carlos@livedesign.org>
2024-10-17 00:24:24 -0500

    Fix lectura

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const path = require('path');
 const { processExcelFile } = require('./lib/excelProcessor');
 
-// Definir la ruta del archivo Excel cargado
-const filePath = path.join(__dirname, 'data/uploads/conjunto_de_datos_new.xlsx');
-const camposPoblacion = ['POBTOT', 'POBFEM', 'POBMAS'];  // Campos dinámicos, pueden variar
-
-// Llamar a la función para procesar el Excel
-processExcelFile(filePath, camposPoblacion)
-  .then(() => console.log('Archivo procesado exitosamente'))
-  .catch((err) => console.error('Error procesando el archivo:', err));
+// Exportar la función para ser usada por otros proyectos
+module.exports = {
+  processExcelFile
+};
