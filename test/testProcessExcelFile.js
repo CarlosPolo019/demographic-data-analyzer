0ade3322e7ad721214aa7e36e68168cbc32897c6
Carlos Manuel Escorcia Polo <carlos@livedesign.org>
2024-10-16 23:26:58 -0500

    add archivo test

---
+++ test/testProcessExcelFile.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const path = require('path');
+const { processExcelFile } = require('../src/excelProcessor');
+
+describe('processExcelFile', function () {
+  it('debería procesar correctamente el archivo Excel', async function () {
+    const filePath = path.join(__dirname, '../data/uploads/conjunto_de_datos_new.xlsx');
+    const camposPoblacion = ['POBTOT', 'POBFEM', 'POBMAS'];
+
+    // Llama a la función y procesa el archivo
+    await processExcelFile(filePath, camposPoblacion);
+
+    // Aquí puedes hacer las aserciones sobre los resultados (si devuelve algo)
+    expect(camposPoblacion).to.be.an('array');
+  });
+});
